d80a859d93beb15740f4acea73d9877209132c3f
rtwayland <rtwayland@gmail.com>
2017-06-29 14:39:15 -0600

    Adding files to barrels for better organization

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,18 +1,22 @@
 import { NgModule } from '@angular/core'
 import { BrowserModule } from '@angular/platform-browser'
 import { AppComponent } from './app.component'
-import { EventsListComponent } from "./events/events-list.component";
-import { EventThumbnailComponent } from "./events/event-thumbnail.component";
+
+import {
+	EventsListComponent,
+	EventThumbnailComponent,
+	EventService,
+	EventDetailsComponent,
+	CreateEventComponent,
+	EventRouteActivator,
+	EventsListResolver
+} from './events/index'
+
 import { NavBarComponent } from "./nav/navbar.component";
-import { EventService } from "./events/shared/event.service";
 import { ToastService } from "./common/toast.service";
-import { EventDetailsComponent } from "./events/event-details/event-details.component";
 import { RouterModule } from "@angular/router";
 import { appRoutes } from "./routes";
-import { CreateEventComponent } from "./events/create-event.component";
 import { Error404Component } from "./errors/404.component";
-import { EventRouteActivator } from "./events/event-details/event-route-activator.service";
-import { EventsListResolver } from "./events/events-list-resolver.service";
 
 @NgModule({
 	imports: [
