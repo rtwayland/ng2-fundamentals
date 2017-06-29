6e4c2235a40624abe9ad3db2731753d4d87b86e9
rtwayland <rtwayland@gmail.com>
2017-06-28 20:47:24 -0600

    Adding user module and route

---
+++ app/user/user.routes.ts
@@ -0,0 +1,5 @@
+import { Routes } from "@angular/router";
+import { ProfileComponent } from "./profile.component";
+export const userRoutes:Routes = [
+	{ path: 'profile', component: ProfileComponent }
+]
