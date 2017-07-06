02ead17fd258c04aa425da548db86ee2851b4009
rtwayland <rtwayland@gmail.com>
2017-07-06 12:07:19 -0600

    Adding a experience level filter

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { ISession } from "../shared/event.model";
 
 @Component({
@@ -6,11 +6,27 @@ import { ISession } from "../shared/event.model";
 	templateUrl: 'app/events/event-details/session-list.component.html'
 })
 
-export class SessionListComponent implements OnInit {
+export class SessionListComponent implements OnInit, OnChanges {
 	@Input() sessions: ISession[]
+	@Input() filterBy: string
+	visibleSessions: ISession[] = []
 	constructor() {
 	}
 
 	ngOnInit() {
+	}
+
+	ngOnChanges() {
+		if(this.sessions) {
+			this.filterSessions(this.filterBy)
+		}
+	}
+
+	filterSessions(filter: string) {
+		if(filter === 'all') {
+			this.visibleSessions = this.sessions.slice(0);
+		} else {
+			this.visibleSessions = this.sessions.filter( session => session.level.toLocaleLowerCase() === filter )
+		}
 	}
 }
