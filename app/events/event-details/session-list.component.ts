b61d6e48b46775f6cfd73c14aff381269135ca0c
rtwayland <rtwayland@gmail.com>
2017-07-06 13:50:17 -0600

    Adding sorting

--- app/events/event-details/session-list.component.ts
+++ app/events/event-details/session-list.component.ts
@@ -9,6 +9,7 @@ import { ISession } from "../shared/event.model";
 export class SessionListComponent implements OnInit, OnChanges {
 	@Input() sessions: ISession[]
 	@Input() filterBy: string
+	@Input() sortBy: string
 	visibleSessions: ISession[] = []
 	constructor() {
 	}
@@ -19,6 +20,9 @@ export class SessionListComponent implements OnInit, OnChanges {
 	ngOnChanges() {
 		if(this.sessions) {
 			this.filterSessions(this.filterBy)
+			this.sortBy === 'name'
+			? this.visibleSessions.sort(sortByNameAsc)
+				: this.visibleSessions.sort(sortByVotesDesc)
 		}
 	}
 
@@ -29,4 +33,16 @@ export class SessionListComponent implements OnInit, OnChanges {
 			this.visibleSessions = this.sessions.filter( session => session.level.toLocaleLowerCase() === filter )
 		}
 	}
+
+
+}
+
+function sortByNameAsc(s1: ISession, s2: ISession) {
+	if (s1.name > s2.name) return 1
+	else if (s1.name === s2.name) return 0
+	else return -1
+}
+
+function sortByVotesDesc(s1: ISession, s2: ISession) {
+	return s2.voters.length - s1.voters.length;
 }
